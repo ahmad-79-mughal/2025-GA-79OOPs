ebb8b8cdb899a8e3396885d68f788c2d16817d16
ahmad-79-mughal <ah721077@gmail.com>
2026-04-19 21:55:34 +0500

    Add files via upload
    OOP Week 4

---
+++ Program (15).cs
@@ -0,0 +1,37 @@
+{
+  "Version": 1,
+  "WorkspaceRootPath": "C:\\Users\\Imran\\OneDrive\\Desktop\\OOP WEEK 4\\task 2\\",
+  "Documents": [
+    {
+      "AbsoluteMoniker": "D:0:0:{43B7337E-6442-4F83-BF44-2D5E8A8D2820}|task 2\\task 2.csproj|c:\\users\\imran\\onedrive\\desktop\\oop week 4\\task 2\\task 2\\program.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{43B7337E-6442-4F83-BF44-2D5E8A8D2820}|task 2\\task 2.csproj|solutionrelative:task 2\\program.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    }
+  ],
+  "DocumentGroupContainers": [
+    {
+      "Orientation": 0,
+      "VerticalTabListWidth": 256,
+      "DocumentGroups": [
+        {
+          "DockedWidth": 200,
+          "SelectedChildIndex": 0,
+          "Children": [
+            {
+              "$type": "Document",
+              "DocumentIndex": 0,
+              "Title": "Program.cs",
+              "DocumentMoniker": "C:\\Users\\Imran\\OneDrive\\Desktop\\OOP WEEK 4\\task 2\\task 2\\Program.cs",
+              "RelativeDocumentMoniker": "task 2\\Program.cs",
+              "ToolTip": "C:\\Users\\Imran\\OneDrive\\Desktop\\OOP WEEK 4\\task 2\\task 2\\Program.cs",
+              "RelativeToolTip": "task 2\\Program.cs",
+              "ViewState": "AgIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-03-20T10:34:00.238Z",
+              "EditorCaption": ""
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+{
+  "Version": 1,
+  "WorkspaceRootPath": "C:\\Users\\Imran\\OneDrive\\Desktop\\OOP WEEK 4\\task 1\\",
+  "Documents": [
+    {
+      "AbsoluteMoniker": "D:0:0:{20B59591-0CA1-4A7C-9A85-4DF176FAAE5E}|task 1\\task 1.csproj|c:\\users\\imran\\onedrive\\desktop\\oop week 4\\task 1\\task 1\\program.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}",
+      "RelativeMoniker": "D:0:0:{20B59591-0CA1-4A7C-9A85-4DF176FAAE5E}|task 1\\task 1.csproj|solutionrelative:task 1\\program.cs||{A6C744A8-0E4A-4FC6-886A-064283054674}"
+    }
+  ],
+  "DocumentGroupContainers": [
+    {
+      "Orientation": 0,
+      "VerticalTabListWidth": 256,
+      "DocumentGroups": [
+        {
+          "DockedWidth": 200,
+          "SelectedChildIndex": 0,
+          "Children": [
+            {
+              "$type": "Document",
+              "DocumentIndex": 0,
+              "Title": "Program.cs",
+              "DocumentMoniker": "C:\\Users\\Imran\\OneDrive\\Desktop\\OOP WEEK 4\\task 1\\task 1\\Program.cs",
+              "RelativeDocumentMoniker": "task 1\\Program.cs",
+              "ToolTip": "C:\\Users\\Imran\\OneDrive\\Desktop\\OOP WEEK 4\\task 1\\task 1\\Program.cs",
+              "RelativeToolTip": "task 1\\Program.cs",
+              "ViewState": "AgIAACYAAAAAAAAAAAAIwC0AAAABAAAAAAAAAA==",
+              "Icon": "ae27a6b0-e345-4288-96df-5eaf394ee369.000738|",
+              "WhenOpened": "2026-03-20T10:31:15.614Z",
+              "EditorCaption": ""
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
